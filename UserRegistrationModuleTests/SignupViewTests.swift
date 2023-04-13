c664e5d645606c6e145bbf6e7444b1150e8ebb53
Asad Mehmood <asadpk80@gmail.com>
2023-04-13 10:57:37 -0400

    one test added in SignupView Tests for input validation

---
+++ UserRegistrationModuleTests/SignupViewTests.swift
@@ -0,0 +1,32 @@
+//
+//  SignupViewTests.swift
+//  UserRegistrationModuleTests
+//
+//  Created by Dev on 4/13/23.
+//
+
+import XCTest
+@testable import UserRegistrationModule
+
+final class SignupViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testSignupView_WhenValidFirstNameProvide_ShouldReturnTrue() {
+        
+        //Arrange
+        let sut = SignupView()
+        
+        //Act
+        let isFirstNameValid = sut.isFirstNameValid("Asad")
+        
+        //Assert
+        XCTAssertTrue(isFirstNameValid, "The isFirstNameValid() method should have returned TRUE for valid first name but has returned FALSE.")
+    }
+}
