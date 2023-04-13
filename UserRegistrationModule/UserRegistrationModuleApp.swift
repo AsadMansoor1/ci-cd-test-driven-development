eeb5ca7500580bdf2ced1258b17bd81075810b89
Asad Mehmood <asadpk80@gmail.com>
2023-04-13 05:35:30 -0400

    project setup

---
+++ UserRegistrationModule/UserRegistrationModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserRegistrationModuleApp.swift
+//  UserRegistrationModule
+//
+//  Created by Dev on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct UserRegistrationModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
